7947fd9175acdbec54b1282b2e19e228fb62d04f
dianlujitao <dianlujitao@lineageos.org>
2025-02-09 20:25:02 +0800

    dodge: Generate fastchg configuration for 23821
    The files are identical on stock except for the project number.
    
    Change-Id: I47e81fff8bba24c93cef35e6d50519b3feee5328

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -13,11 +17,18 @@ namespace_imports = [
     'vendor/oneplus/sm8750-common',
 ]
 
+blob_fixups: blob_fixups_user_type = {
+    (
+        'odm/firmware/fastchg/23821/charging_hyper_mode_config.txt',
+        'odm/firmware/fastchg/23821/single_charging_hyper_mode_config.txt',
+    ): blob_fixup().regex_replace(r"(PROJECT:=)23893", r"\g<1>23821"),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'dodge',
     'oneplus',
     namespace_imports=namespace_imports,
-    # blob_fixups=blob_fixups,
+    blob_fixups=blob_fixups,
     # add_firmware_proprietary_file=True,
 )
 
