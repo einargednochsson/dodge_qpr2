298fc4676b406be7b584399924d79ca993046aa0
dianlujitao <dianlujitao@lineageos.org>
2025-08-05 12:55:59 +0800

    dodge: Set device model for IFAA
    Change-Id: I7828355fbb0eb97c8985f53886612d1f30322249

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,7 @@ TARGET_POWERHAL_MODE_EXT_LIB := power-ext-oplus
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
