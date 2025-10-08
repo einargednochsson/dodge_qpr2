085f3b3ca7fe484571f8e9d2aca9afaa723d540f
Bruno Martins <bgcngm@gmail.com>
2025-10-08 23:21:41 +0100

    dodge: Update from OOS 11.C.70
    Change-Id: Iff7744653db549efdec86306bba290442a6d48e8

--- lineage_dodge.mk
+++ lineage_dodge.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := CPH2653
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 15 AP3A.240617.008 1756201541239 release-keys" \
-    BuildFingerprint=OnePlus/CPH2653EEA/OP5D55L1:15/AP3A.240617.008/V.R4T3.38e5dcd-19631d6-1977a50:user/release-keys \
+    BuildDesc="qssi_64-user 15 AP3A.240617.008 1759672914946 release-keys" \
+    BuildFingerprint=OnePlus/CPH2653EEA/OP5D55L1:15/AP3A.240617.008/V.R4T3.1accb8e_566b33_566b34:user/release-keys \
     DeviceName=OP5D55L1 \
     DeviceProduct=CPH2653 \
     SystemDevice=OP5D55L1 \
